6ad07208d5a302b7eee9e9a5043b41cb0a04912f
Administrator <test123>
2016-06-28 16:08:39 +0800

    v.03 教程配套源码

---
+++ js/base.js
@@ -0,0 +1,41 @@
+var $ = function(){
+    return new Base();
+};
+function Base (){
+    //创建数组，来保存获得的节点和节点数组
+    this.elements =[];
+    //获取ID节点
+    this.getId = function(id){
+        this.elements.push(document.getElementById(id));
+        return this;
+    }
+    //获取元素节点
+    this.getTagName = function(tag){
+        var tags = document.getElementsByTagName(tag);
+        for(var i=0;i<tags.length;i ++){
+            this.elements.push(tags[i]);
+        }
+        return this;
+    }
+}
+
+Base.prototype.css = function(attr,value){
+    for(var i=0;i<this.elements.length;i ++){
+        this.elements[i].style[attr] = value;
+    }
+    return this;
+}
+
+Base.prototype.html = function(str){
+    for(var i=0;i<this.elements.length;i ++){
+        this.elements[i].innerHTML = str;
+    }
+    return this;
+}
+
+Base.prototype.click = function(fn){
+    for(var i=0;i<this.elements.length;i ++){
+        this.elements[i].onclick = fn;
+    }
+    return this;
+}
---
+++ js/demo.js
@@ -0,0 +1,7 @@
+window.onload =function(){
+    //var base = new Base();
+    $().getId('box').css("backgroundColor",'gray').html('БъЬт');
+    $().getTagName('p').css("color",'green').click(function(){
+        alert('a');
+    });
+};
